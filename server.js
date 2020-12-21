413283f61c29ca3364b7c256fa673af80b8da872
uros87 <uros_brkic@yahoo.com>
2020-12-21 13:51:28 +0100

    ADD:Mongo DB instance and connection to it

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const express = require('express');
-const dotenv = require('dotenv');
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
